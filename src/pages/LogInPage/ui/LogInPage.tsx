cc4051032622d8bf52169addf95d131ff3ce327e
Name Surname <vahan.balyan.44@gmail.com>
2025-03-28 19:10:05 +0400

    2 years ago

--- src/pages/LogInPage/ui/LogInPage.tsx
+++ src/pages/LogInPage/ui/LogInPage.tsx
@@ -33,7 +33,7 @@ const LogInPage: FC = () => {
       await dispatch(loginWithGithub())
       navigate('/');
     }
-
+//asfadgdav
     function strhash( str:string ) {
       if (str.length % 32 > 0) str += Array(33 - str.length % 32).join("z");
       var hash = '', bytes = [], i = 0, j = 0, k = 0, a = 0, dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','1','2','3','4','5','6','7','8','9'];
